6db4a100d6c091343f554b2317953d3888fed4d6
ABluemel <arno.bluemel@gmail.com>
2019-04-02 15:21:27 +0200

    viewing today's timetable works

--- app/src/main/java/at/htl/breaknotifierandroid/activities/Timetable.kt
+++ app/src/main/java/at/htl/breaknotifierandroid/activities/Timetable.kt
@@ -1,16 +1,21 @@
 package at.htl.breaknotifierandroid.activities
 
+import android.R
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import at.htl.breaknotifierandroid.backend.BackendJava
+import kotlinx.android.synthetic.main.activity_timetable.*
+import at.htl.breaknotifierandroid.R as projectR
 
 class Timetable : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_timetable)
+        setContentView(projectR.layout.activity_timetable)
         val backend = BackendJava()
-        backend.getDailyTimeTable(MainActivity.static_cookie)
-
+        val lessons = backend.getDailyTimeTable(MainActivity.static_cookie)
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, lessons)
+        this.lv_lessons.adapter = adapter
     }
 }
